5c66f9ebe7156b609c0f352f9a30726b3bbc1cb5
Cyrus <hardisoncyrus@gmail.com>
2023-01-28 20:33:22 +0800

    first commit

---
+++ app/src/main/java/iss/ad/team6/sharefood/MainActivity.java
@@ -0,0 +1,14 @@
+package iss.ad.team6.sharefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
